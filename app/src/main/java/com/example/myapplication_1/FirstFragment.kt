12ed17e8fb5426a26365cf3b532cdde9cc9bd9e3
kyung <kyungha1227@naver.com>
2021-07-02 10:26:40 +0900

    multiTab+Contact+Gallery

--- app/src/main/java/com/example/myapplication_1/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication_1/FirstFragment.kt
@@ -1,29 +1,40 @@
-package com.example.myapplication_1
+  package com.example.myapplication_1
 
+import android.database.Cursor
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var cursor : Cursor? = requireActivity().contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null)
+        activity?.startManagingCursor(cursor)
+
+        var from = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone._ID)
+        var to = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        var simple : SimpleCursorAdapter = SimpleCursorAdapter(activity, android.R.layout.simple_list_item_2, cursor, from, to)
+        val listview = view?.findViewById(R.id.listView) as ListView
+        listview.setAdapter(simple)
+    }
+
 
     fun newInstant():FirstFragment{
         val args = Bundle()
