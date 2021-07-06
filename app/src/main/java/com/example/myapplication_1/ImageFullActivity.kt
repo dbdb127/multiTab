7a3e17ba4b5c125963027065f032a7b4a21d0bfb
kyung <kyungha1227@naver.com>
2021-07-06 11:13:25 +0900

    Image Zoom

--- app/src/main/java/com/example/myapplication_1/ImageFullActivity.kt
+++ app/src/main/java/com/example/myapplication_1/ImageFullActivity.kt
@@ -9,45 +9,18 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 
 class ImageFullActivity : AppCompatActivity() {
-    //image zoom parameter
-    private var mScaleGestureDetector: ScaleGestureDetector?=null
-    private var scaleFactor=1.0f
-    private lateinit var mImageView:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_full)
 
-        val imagePath=intent.getStringExtra("path")
-        val imageName=intent.getStringExtra("name")
+        val imagePath = intent.getStringExtra("path")
+        val imageName = intent.getStringExtra("name")
 
         var textView = findViewById<TextView>(R.id.textView)
         textView.text = imageName
         Glide.with(this)
             .load(imagePath)
             .into(findViewById(R.id.imageView))
-
-        //image zoom
-        mImageView = findViewById(R.id.imageView)
-        mScaleGestureDetector = ScaleGestureDetector(this, ScaleListener())
-
     }
-
-    override fun onTouchEvent(motionEvent: MotionEvent?): Boolean {
-        mScaleGestureDetector!!.onTouchEvent(motionEvent)
-        return true
-    }
-
-    inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener(){
-        override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean {
-            scaleFactor*=scaleGestureDetector.scaleFactor
-
-            scaleFactor = Math.max(0.5f, Math.min(scaleFactor, 2.0f))
-
-            mImageView.scaleX = scaleFactor
-            mImageView.scaleY = scaleFactor
-            return true
-        }
-    }
-
 }
